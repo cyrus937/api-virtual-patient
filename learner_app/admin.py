7e977c0bd08980e3a56c871f9f41b2788fa95985
cyrus937 <cyrillekenfack4@gmail.com>
2022-06-16 12:19:27 +0100

    update learner

--- learner_app/admin.py
+++ learner_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Symptom)
+admin.site.register(Disease)
+admin.site.register(WeightSymptomDisease)
+admin.site.register(System)
+admin.site.register(WeightDiseaseSystem)
+admin.site.register(RatingDisease)
+admin.site.register(RatingSystem)
--- learner_app/views.py
+++ learner_app/views.py
@@ -1,5 +1,6 @@
 from asyncio import constants
 from audioop import avg
+from pickle import FALSE
 from unicodedata import decimal
 from django.shortcuts import redirect
 from patient_app.models import LeanerPhysician
@@ -16,6 +17,8 @@ from decimal import Decimal
 
 from .models import *
 from .serializers import *
+
+from math import sqrt
 
 # Create your views here.
 class SymptomViewSet(viewsets.ModelViewSet):
@@ -46,8 +49,7 @@ class RatingSystemViewSet(viewsets.ModelViewSet):
   queryset = RatingSystem.objects.all()
   serializer_class = RatingSystemSerializer
 
-@api_view(['GET'])
-def getratingUser(request, pk):
+def getRating(request, pk):
   systems = {}
   diseases = {}
   det = {}
@@ -85,18 +87,61 @@ def getratingUser(request, pk):
     
     #rating_disease = RatingDiseaseSerializer(RatingDisease.objects.all().filter(learner=learner["id"]), many=True, context=context).data
     rating_disease = list(Disease.objects.annotate(rating=Sum('ratingdisease__rating')).values('name', 'rating', 'ratingdisease__learner').filter(ratingdisease__learner=pk))
-    """for dis in rating_disease:
-      dis = Convert(dis, {})
-      disease = DiseaseSerializer(Disease.objects.get(pk=dis["disease"]), many=False, context=context).data
-      diseases[disease["name"]] = dis["rating"]"""
+    
     for dis in rating_disease:
       diseases[dis["name"]] = dis["rating"]
     
-    result = {
-      "system":systems if systems else None,
-      "disease":diseases if diseases else None,
-      "detail":details if details else None
+  return systems, diseases, details
+
+@api_view(['GET'])
+def getratingUser(request, pk):
+  """ systems = {}
+  diseases = {}
+  det = {}
+  details = {}
+  context = {
+        'request': request,
     }
+
+  try: 
+    learner = LeanerPhysicianSerializer(LeanerPhysician.objects.get(pk=pk), many=False, context=context).data
+  except LeanerPhysician.DoesNotExist: 
+    return JsonResponse({'message': 'The learner does not exist'}, status=status.HTTP_404_NOT_FOUND)
+  if request.method == 'GET':
+
+    rating_system = RatingSystemSerializer(RatingSystem.objects.all().filter(learner=learner["id"]), many=True, context=context).data
+
+    for sys in rating_system:
+      sys = Convert(sys, {})
+      s = sys["system"].split('/')
+      syst = SystemSerializer(System.objects.get(pk=s[-2]), many=False, context=context).data
+      systems[syst["name"]] = sys["rating"]
+
+      det["rating"] = sys["rating"]
+      diseases_rating = RatingDiseaseSerializer(RatingDisease.objects.all().filter(system=s[-2], learner=learner["id"]), many=True, context=context).data
+      for dis_rate in diseases_rating:
+        dis_rate = Convert(dis_rate, {})
+        d = dis_rate["disease"].split('/')
+        dis = DiseaseSerializer(Disease.objects.get(pk=d[-2]), many=False, context=context).data
+        det[dis["name"]] = dis_rate["rating"]
+      
+      details[syst["name"]] = det
+
+      det = {}
+
+    
+    #rating_disease = RatingDiseaseSerializer(RatingDisease.objects.all().filter(learner=learner["id"]), many=True, context=context).data
+    rating_disease = list(Disease.objects.annotate(rating=Sum('ratingdisease__rating')).values('name', 'rating', 'ratingdisease__learner').filter(ratingdisease__learner=pk))
+    
+    for dis in rating_disease:
+      diseases[dis["name"]] = dis["rating"] """
+    
+  systems, diseases, details = getRating(request=request, pk=pk)
+  result = {
+    "system":systems if systems else None,
+    "disease":diseases if diseases else None,
+    "detail":details if details else None
+  }
 
   return Response(result, status=status.HTTP_200_OK)
 
@@ -138,10 +183,10 @@ def rating(request):
         "message": "symptoms required"
       }
       return Response(result, status.HTTP_204_NO_CONTENT)
-    if 'loss' not in body:
+    if 'status' not in body:
       result = {
         "status": "FAILURE",
-        "message": "loss required"
+        "message": "status required"
       }
       return Response(result, status.HTTP_204_NO_CONTENT)
   
@@ -149,62 +194,99 @@ def rating(request):
     learner = LeanerPhysician.objects.get(pk=body["learner"])
   except LeanerPhysician.DoesNotExist: 
     return JsonResponse({'message': 'The learner does not exist'}, status=status.HTTP_404_NOT_FOUND)
-
-  sys = System.objects.get(pk=body["system"])
-  for disease in body["disease"] :
-    try:
-      weight_disease = Decimal(WeightDiseaseSystemSerializer(WeightDiseaseSystem.objects.get(disease=disease, system=body["system"]), many=False, context=context).data["weight"])
-    except WeightDiseaseSystem.DoesNotExist:
-      continue
-    
-    for symptom in body["symptoms"]:
+  
+  if body["status"]:
+    disease = body["disease"]
+    for system in body["system"] :
+      sys = System.objects.get(pk=system)
+      rate_disease = 0
+      new = FALSE
       try:
-        weight_symptom = WeightSymptomDiseaseSerializer(WeightSymptomDisease.objects.get(disease=disease, symptom=symptom), many=False, context=context).data
-      except WeightSymptomDisease.DoesNotExist:
+        weight_disease = Decimal(WeightDiseaseSystemSerializer(WeightDiseaseSystem.objects.get(disease=disease, system=system), many=False, context=context).data["weight"])
+      except WeightDiseaseSystem.DoesNotExist:
         continue
       
-      weight_symptom = Decimal(weight_symptom["weight"])
+      for symptom in body["symptoms"]:
+        try:
+          weight_symptom = WeightSymptomDiseaseSerializer(WeightSymptomDisease.objects.get(disease=disease, symptom=symptom), many=False, context=context).data
+        except WeightSymptomDisease.DoesNotExist:
+          continue
+        
+        rate_disease += Decimal(weight_symptom["weight"])
       try:
-        rating_disease = RatingDisease.objects.get(disease=disease, system=body["system"], learner=body["learner"])
-        rating_disease.rating += weight_symptom
-        rating_disease.rating = rating_disease.rating
+        rating_disease = RatingDisease.objects.get(disease=disease, system=system, learner=body["learner"])
+        rate = rating_disease.rating
+        if rate == 0.0:
+          rating_disease.rating = 0.1
+        else:
+          rating_disease.rating = round(sqrt(rate * rate_disease), 3)
         rating_disease.save()
       except RatingDisease.DoesNotExist:
+        new = True
         dis = Disease.objects.get(pk=disease)
         rating_disease = RatingDisease.objects.create(
-          rating = weight_symptom,
+          rating = round(rate_disease, 3),
           disease = dis,
           learner = learner,
           system = sys
         )
         rating_disease.save()
+        
+      try:
+        rating_system = RatingSystem.objects.get(system=system, learner=body["learner"])
+        if new:
+          rating_system.rating += rating_disease.rating * weight_disease
+        else:
+          rating_system.rating -= rate * weight_disease
+          rating_system.rating += round(rating_disease.rating * weight_disease, 3)
+        rating_system.save()
+      except RatingSystem.DoesNotExist:
+        rating_system = RatingSystem.objects.create(
+          rating = round(rating_disease.rating * weight_disease, 3),
+          system = sys,
+          learner = learner
+        )
+        rating_system.save()
+  else:
+    disease = body["disease"]
+    for system in body["system"] :
+      sys = System.objects.get(pk=system)
+      new = FALSE
     
-    for symptom in body["loss"]:
       try:
-        weight_symptom = WeightSymptomDiseaseSerializer(WeightSymptomDisease.objects.get(disease=disease, symptom=symptom), many=False, context=context).data
-      except WeightSymptomDisease.DoesNotExist:
-        continue
-      
-      weight_symptom = Decimal(weight_symptom["weight"])
-      try:
-        rating_disease = RatingDisease.objects.get(disease=disease, system=body["system"], learner=body["learner"])
-        rating_disease.rating -= weight_symptom
-        rating_disease.rating = rating_disease.rating if rating_disease.rating>=0 else 0
+        rating_disease = RatingDisease.objects.get(disease=disease, system=system, learner=body["learner"])
+        rate = rating_disease.rating - 0.1
+        if rate <= 0.0:
+          rating_disease = 0.0
+        else:
+          rating_disease.rating = round(rate, 3)
         rating_disease.save()
       except RatingDisease.DoesNotExist:
-        continue
+        new = True
+        dis = Disease.objects.get(pk=disease)
+        rating_disease = RatingDisease.objects.create(
+          rating = 0.00,
+          disease = dis,
+          learner = learner,
+          system = sys
+        )
+        rating_disease.save()
       
-    try:
-      rating_system = RatingSystem.objects.get(system=body["system"], learner=body["learner"])
-      rating_system.rating += rating_disease.rating * weight_disease
-      rating_system.save()
-    except RatingSystem.DoesNotExist:
-      rating_system = RatingSystem.objects.create(
-        rating = round(rating_disease.rating * weight_disease, 3),
-        system = sys,
-        learner = learner
-      )
-      rating_system.save()
+      try:
+        rating_system = RatingSystem.objects.get(system=system, learner=body["learner"])
+        if new:
+          rating_system.rating += rating_disease.rating * weight_disease
+        else:
+          rating_system.rating -= rate * weight_disease
+          rating_system.rating += round(rating_disease.rating * weight_disease, 3)
+        rating_system.save()
+      except RatingSystem.DoesNotExist:
+        rating_system = RatingSystem.objects.create(
+          rating = round(rating_disease.rating * weight_disease, 3),
+          system = sys,
+          learner = learner
+        )
+        rating_system.save()
   
   result = {
       "message": "rating success",
